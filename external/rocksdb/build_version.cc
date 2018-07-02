8d8b7d3921dcd174231891dc322a2db87cbd3cbc
nanzee <nanze@xpool.zone>
2018-07-03 04:33:54 +0530

    RocksDB upgraded to v5.12.2

---
+++ external/rocksdb/build_version.cc
@@ -0,0 +1,4 @@
+#include "build_version.h"
+const char* rocksdb_build_git_sha = "rocksdb_build_git_sha:@0@";
+const char* rocksdb_build_git_date = "rocksdb_build_git_date:@2018/07/02 22:42:04@";
+const char* rocksdb_build_compile_date = __DATE__;
