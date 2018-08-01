ed2a2c23ec2602287987093b86a82d8d3f7c3477
nanzee <nanze@xpool.zone>
2018-08-01 23:28:23 +0530

    Added upgrade heights for hardfork;

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -79,8 +79,8 @@ const size_t   FUSION_TX_MIN_INPUT_COUNT                     = 12;
 const size_t   FUSION_TX_MIN_IN_OUT_COUNT_RATIO              = 4;
 
 const uint32_t KEY_IMAGE_CHECKING_BLOCK_INDEX                = 0;
-const uint32_t UPGRADE_HEIGHT_V2                             = 74999;
-const uint32_t UPGRADE_HEIGHT_V3                             = 75000;
+const uint32_t UPGRADE_HEIGHT_V2                             = 84000;
+const uint32_t UPGRADE_HEIGHT_V3                             = 85000;
 const unsigned UPGRADE_VOTING_THRESHOLD                      = 90;               // percent
 const uint32_t UPGRADE_VOTING_WINDOW                         = EXPECTED_NUMBER_OF_BLOCKS_PER_DAY;  // blocks
 const uint32_t UPGRADE_WINDOW                                = EXPECTED_NUMBER_OF_BLOCKS_PER_DAY;  // blocks
@@ -127,7 +127,10 @@ const uint64_t P2P_DEFAULT_INVOKE_TIMEOUT                    = 60 * 2 * 1000; //
 const size_t   P2P_DEFAULT_HANDSHAKE_INVOKE_TIMEOUT          = 5000;          // 5 seconds
 const char     P2P_STAT_TRUSTED_PUB_KEY[]                    = "";
 
-const char* const SEED_NODES[] = { "45.77.169.160:31569", "149.28.128.117:31569" };
+const char* const SEED_NODES[] = { 
+                                   "45.77.169.160:31569", 
+                                   "149.28.128.117:31569" 
+                                   };
 
 
 struct CheckpointData {
@@ -136,10 +139,11 @@ struct CheckpointData {
 };
 
 const std::initializer_list<CheckpointData> CHECKPOINTS = { 
-  {16000, "ca608aa8dc6cc93a409a5363b5fe0adadaa507d09d1b6e133aae4262f132f41d"}
-, {22000, "8242cad741c0cb6286931eedfcd0c20f997524489120c91cc59f67754fb9782e"}
-, {42000, "22439349e3c82117676e8b37d3f9b3181a918eebebbf8134a6195cbb792c3590"}
-, {65000, "5afa296a21f236c584d6ec3036abd525aaf7170f57f5ae15cb562d6522d1ad76"}
+  {16000, "ca608aa8dc6cc93a409a5363b5fe0adadaa507d09d1b6e133aae4262f132f41d"}, 
+  {22000, "8242cad741c0cb6286931eedfcd0c20f997524489120c91cc59f67754fb9782e"}, 
+  {42000, "22439349e3c82117676e8b37d3f9b3181a918eebebbf8134a6195cbb792c3590"}, 
+  {65000, "5afa296a21f236c584d6ec3036abd525aaf7170f57f5ae15cb562d6522d1ad76"},
+  {74000, "1fc27d05366acb5600fc2bfad3ce989417c129e837ebba39ac4131b7cbfd6091"}
  };
 
 } // CryptoNote
